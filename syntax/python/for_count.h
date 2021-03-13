b5c68491558ed646e0fcdb1317d1ac5616632257
CyTon Code <cytoncode@gmail.com>
2021-03-13 17:03:06 +0200

    upd all

--- syntax/python/for_count.h
+++ syntax/python/for_count.h
@@ -1,24 +1,28 @@
 //
-// Created by cytoncode on 13.03.2021.
+// Created by cyton code on 13.03.2021.
 //
 
-#ifndef CPPLEARNING_FOR_COUNT_H
-#define CPPLEARNING_FOR_COUNT_H
+#ifndef CPP_LEARNING_FOR_COUNT_H
+#define CPP_LEARNING_FOR_COUNT_H
 /**
  * args - массив.
  * arg - итератор.
  * n - количество элементов.
  * count - счетчик.
  * */
-#define FOR_COUNT(n, list, i, count) count = 0;  \
-for (auto i = list[0]; count < n; count++, i = list[count])
+#define FOR_COUNT(list, i, start, stop, step)   \
+for (auto i = list[0]; start < stop; start+=step, i = list[start])
+
+
+int start = 0, stop = 0, step = 1;
+//int range[3]= {0, 0, 1};
 
 void print(int n, char **args) {
-    int count;
-    FOR_COUNT(n, args, arg, count) {
+    start = 0;
+    FOR_COUNT(args, arg, start, n, step) {
         cout << arg << " ";
     }
     cout << endl;
 }
 
-#endif //CPPLEARNING_FOR_COUNT_H
+#endif //CPP_LEARNING_FOR_COUNT_H
