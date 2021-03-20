b5e1f63ddada198f9a8addc2f69423f2e8ac5087
CyTon Code <cytoncode@gmail.com>
2021-03-20 17:15:18 +0200

    add all

---
+++ lib/scripts/list/len/cmake-build-debug/CMakeFiles/len.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/len.dir/library.cpp.o"
+  "liblen.pdb"
+  "liblen.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/len.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/scripts/list/len/library.h
@@ -0,0 +1,6 @@
+#ifndef LEN_LIBRARY_H
+#define LEN_LIBRARY_H
+
+void hello();
+
+#endif //LEN_LIBRARY_H
