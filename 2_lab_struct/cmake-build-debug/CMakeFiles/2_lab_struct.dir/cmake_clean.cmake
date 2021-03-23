27b2692d97ebd3dc7f653b1d91b9d4fa63a64b57
CyTon Code <cytoncode@gmail.com>
2021-03-23 13:38:00 +0200

    add all
    Signed-off-by: CyTon Code <cytoncode@gmail.com>

---
+++ 2_lab_struct/cmake-build-debug/CMakeFiles/2_lab_struct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_lab_struct"
+  "2_lab_struct.pdb"
+  "CMakeFiles/2_lab_struct.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_lab_struct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
